6805fe7f8a58015da26bd4eface26e6652a6d7a2
VincentRenaud36 <120531889+VincentRenaud36@users.noreply.github.com>
2025-03-03 19:21:36 +0100

    tentative pour githubPages

--- alvea/next.config.ts
+++ alvea/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  output: "export", // On utilise la génération statique
+
+  basePath: "/alvea", // On définit le chemin de base
+
   images: {
+    unoptimized: true, // On désactive l'optimisation des images
     domains: ['img.youtube.com'], // On autorise le domaine pour charger les images
   },
 };
