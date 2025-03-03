dcde2759ed61c127b3b7f8ecbd8cd7dc70ff1ad4
GaLaHyClear <66382758+GaLaHyClear@users.noreply.github.com>
2025-03-03 09:27:48 +0100

    update image

--- alvea/src/app/components/VideoPodcast.tsx
+++ alvea/src/app/components/VideoPodcast.tsx
@@ -6,15 +6,15 @@ import Link from 'next/link';
 
 function VideoPodcast() {
     const mentors = [
-        { id: 1, name: "Annette Black", role: "Dog Trainer", image: "/Images/1.PNG" },
-        { id: 2, name: "Robert Green", role: "Life Coach", image: "/Images/2.PNG" },
-        { id: 3, name: "Marie Durant", role: "Business Mentor", image: "/Images/3.PNG" },
-        { id: 4, name: "Marie Durant", role: "Business Mentor", image: "/Images/4.PNG" },
-        { id: 5, name: "Marie Durant", role: "Business Mentor", image: "/Images/5.PNG" },
-        { id: 6, name: "Marie Durant", role: "Business Mentor", image: "/Images/6-1.PNG" },
-        { id: 7, name: "Marie Durant", role: "Business Mentor", image: "/Images/6-2.PNG" },
-        { id: 8, name: "Marie Durant", role: "Business Mentor", image: "/Images/7-1.PNG" },
-        { id: 9, name: "Marie Durant", role: "Business Mentor", image: "/Images/7-2.PNG" },
+        { id: 1, name: "Jean Lambert", role: "Vendeur de marron", image: "/Images/1.PNG" },
+        { id: 2, name: "Camille Moreau", role: "Couturière", image: "/Images/2.PNG" },
+        { id: 3, name: "Ismaël Laurens", role: "Libraire", image: "/Images/3.PNG" },
+        { id: 4, name: "Bernard Alban", role: "Vendeur", image: "/Images/4.PNG" },
+        { id: 5, name: "Jeanne Alliot", role: "Couturière", image: "/Images/5.PNG" },
+        { id: 6, name: "Arthur Fisher", role: "Chocolatier", image: "/Images/6-1.PNG" },
+        { id: 7, name: "Pierre Stone", role: "Barista", image: "/Images/6-2.PNG" },
+        { id: 8, name: "Marie Durant", role: "Vendeur", image: "/Images/7-1.PNG" },
+        { id: 9, name: "Amir Durant", role: "Vendeur", image: "/Images/7-2.PNG" },
     ];
 
     const column1 = mentors.slice(0, 3);
@@ -32,7 +32,7 @@ function VideoPodcast() {
                     {/* Colonne 1 */}
                     <div className="flex flex-col items-center">
                         {column1.map((mentor) => (
-                            <div key={mentor.id} className="relative rounded-lg overflow-hidden">
+                            <div key={mentor.id} className="relative rounded-lg overflow-hidden mt-4">
                                 <Image
                                     src={mentor.image}
                                     alt={`Photo de ${mentor.name}`}
@@ -47,7 +47,7 @@ function VideoPodcast() {
                     {/* Colonne 2 */}
                     <div className="flex flex-col items-center mt-20">
                         {column2.map((mentor) => (
-                            <div key={mentor.id} className="relative rounded-lg overflow-hidden">
+                            <div key={mentor.id} className="relative rounded-lg overflow-hidden mt-4">
                                 <Image
                                     src={mentor.image}
                                     alt={`Photo de ${mentor.name}`}
@@ -62,7 +62,7 @@ function VideoPodcast() {
                     {/* Colonne 3 */}
                     <div className="flex flex-col items-center mt-40">
                         {column3.map((mentor) => (
-                            <div key={mentor.id} className="relative rounded-lg overflow-hidden">
+                            <div key={mentor.id} className="relative rounded-lg overflow-hidden mt-4">
                                 <Image
                                     src={mentor.image}
                                     alt={`Photo de ${mentor.name}`}
